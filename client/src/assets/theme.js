d6abecf7cf4f7271a223b07bbda46a7f6e7fd517
Hoanghuygia <hoanghuy051230@gmail.com>
2024-05-10 09:35:14 +0700

    divide theme to another file

---
+++ client/src/assets/theme.js
@@ -0,0 +1,13 @@
+const Colors = {
+    'bg-color': {
+        100: '#fef9f8',
+        200: '#efd1d8',
+        300: '#efcacc'
+    },
+    'text-color': {
+        100: '#cac3e4',
+        200: '#d6cae3'
+    }  
+}
+
+export{Colors}
--- client/src/main.jsx
+++ client/src/main.jsx
@@ -1,32 +1,17 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import { ChakraProvider, extendTheme } from "@chakra-ui/react";
-import {BrowserRouter} from "react-router-dom";
 import App from "./App.jsx";
+import {Colors} from "./assets/theme.js";
 import "./index.css";
 
-const colors = {
-    'bg-color': {
-        100: '#fef9f8',
-        200: '#efd1d8',
-        300: '#efcacc'
-    },
-    'text-color': {
-        100: '#cac3e4',
-        200: '#d6cae3'
-    }
-    
-}
-
-const theme = extendTheme({colors});
+const theme = extendTheme({Colors});
 
 ReactDOM.createRoot(document.getElementById("root")).render(
     <React.StrictMode>
-        {/* <BrowserRouter> */}
             <ChakraProvider theme={theme}>
                 <App />
             </ChakraProvider>
-        {/* </BrowserRouter> */}
     </React.StrictMode>
 );
 
