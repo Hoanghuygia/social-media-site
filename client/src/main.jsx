b68938c6c81b54d2a29f65f3ef7f239bb88a6629
Hoanghuygia <hoanghuy051230@gmail.com>
2024-05-13 02:06:02 +0700

    fix theme file, add header component and fix tailwind css problem

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -1,11 +1,9 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-import { ChakraProvider, extendTheme } from "@chakra-ui/react";
+import theme from './assets/theme.js'
+import { ChakraProvider} from "@chakra-ui/react";
 import App from "./App.jsx";
-import {Colors} from "./assets/theme.js";
-import "./index.css";
-
-const theme = extendTheme({Colors});
+import './index.css'
 
 ReactDOM.createRoot(document.getElementById("root")).render(
     <React.StrictMode>
