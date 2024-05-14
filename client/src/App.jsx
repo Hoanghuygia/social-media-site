4f84ac072dabf550ffabfbfb2d6bd5eff2171744
Illyamiyu <animeseed0@gmail.com>
2024-05-14 12:23:36 +0700

    add log in sign in

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,26 +1,33 @@
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import { publicRoutes } from './routes/index.js';
-import { Fragment } from 'react';
+import { Fragment, useState } from 'react';
+import LoginPage from './LoginSignin/LoginPage.jsx';
 
 
 function App() {
-  return (
-      <Router>
-	          <Routes>
-	              {publicRoutes.map((route, index) =>{
-					const Page = route.component;
-					
-					let Layout;
-					if(route.layout === null)
-						Layout = Fragment;
-					else
-						Layout = route.layout;
-					return <Route key={index} path={route.path} element={<Layout><Page/></Layout>}/>
-				  })};
-	          </Routes>
-      </Router>
+	const [isLogin, setIsLogin] = useState(false);
 
-    )
+	if (isLogin)
+		return (
+			<Router>
+					<Routes>
+						{publicRoutes.map((route, index) =>{
+							const Page = route.component;
+							
+							let Layout;
+							if(route.layout === null)
+								Layout = Fragment;
+							else
+								Layout = route.layout;
+							return <Route key={index} path={route.path} element={<Layout><Page/></Layout>}/>
+						})};
+					</Routes>
+			</Router>
+    	);
+	else
+		return (
+			<LoginPage Login={setIsLogin}/>
+		);
 }
 
 
