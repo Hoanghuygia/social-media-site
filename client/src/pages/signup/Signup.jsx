61be46f924dd2a587e3e3489f9c1ecb11c0573d5
Illyamiyu <animeseed0@gmail.com>
2024-05-20 19:22:13 +0700

    Fix login signup

--- client/src/pages/signup/Signup.jsx
+++ client/src/pages/signup/Signup.jsx
@@ -1,6 +1,17 @@
+import SigninForm from "./SigninForm";
+
 function Signup() {
-    return (
-        <h1>Sign up</h1>
+    const style = {
+        backgroundColor: "rgb(254, 249, 248)"
+    }
+
+    return(
+        <>
+            <div className="min-w-screen  min-h-screen	w-auto	h-auto " style={style}>
+                <SigninForm />
+                <img src="/img/Sugarcube.png" alt="Picture of creature and word" className="absolute w-1/3	left-32 top-20" />
+            </div>
+        </>
     );
 }
 
