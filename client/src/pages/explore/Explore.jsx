e3d50ec1e7590547aa0729f22ef25c4009a74df3
Hoanghuygia <hoanghuy051230@gmail.com>
2024-05-22 19:28:14 +0700

    change icon for navbar and create message component

--- client/src/pages/explore/Explore.jsx
+++ client/src/pages/explore/Explore.jsx
@@ -1,5 +1,25 @@
+import { Flex, Box, VStack } from "@chakra-ui/react";
+
 function Explore() {
-    return ( <h2>Explore Page</h2>);
+    return (
+        <VStack flexDir={"column"} align={"left"} position="relative">
+            <Box bg="yellow" width="150px" overflowWrap='break-word' wordBreak='break-all'>
+                jkkkkkkkkkkknkkkjkjjjjhjjjjjjjjjjjjjjjjjj
+            </Box>
+            <Box bg="green" width="auto" >
+                njjkkkbnnnnnnnnnnnnnn
+            </Box>
+            <Box
+                bg="pink.200"
+                width="auto"
+                className="third"
+                minWidth="fit-content"
+
+            >
+                hkkkkk
+            </Box>
+        </VStack>
+    );
 }
 
 export default Explore;
