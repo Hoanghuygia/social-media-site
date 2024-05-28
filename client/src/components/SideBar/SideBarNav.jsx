fa43746c7a8a33bb5dee592fa7f9c6dd7645396e
Hoanghuygia <hoanghuy051230@gmail.com>
2024-05-28 12:34:01 +0700

    update link

--- client/src/components/SideBar/SideBarNav.jsx
+++ client/src/components/SideBar/SideBarNav.jsx
@@ -9,7 +9,7 @@ function SideBarNav() {
         <Flex flexDir="column" gap={3} mt="10px">
             <Bar icon={HiOutlineHome} barName={"Home"} route={"/"}/>
             <Bar icon={HiOutlineChatBubbleOvalLeftEllipsis } barName={"Message"} route={"/message"}/>
-            <Bar icon={HiOutlineGlobeAlt} barName={"Explore"} route={"/explore"}/>
+            <Bar icon={HiOutlineGlobeAlt} barName={"Explore"} route={"/explore/reels"}/>
             <Bar icon={HiOutlineBellAlert} barName={"Notification"} route={"/notification"}/>
             <Bar icon={HiOutlineUser} barName={"Profile"} route={"/profile"}/>
         </Flex>
