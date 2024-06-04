1aca48df43b5f4c83a0afaf2d605a83e52677a58
Hoanghuygia <hoanghuy051230@gmail.com>
2024-06-05 00:24:58 +0700

    reset ngtram branch

--- api/src/app/models/user.js
+++ api/src/app/models/user.js
@@ -1,14 +1,65 @@
-const { default: mongoose } = require("mongoose");
+const mongoose = require("mongoose");
+const { isEmail } = require("validator");
 
-const Schema = mongoose.Schema;
-
-const User = new Schema(
-    {
-        username: {
-            type: String,
-            minLength: 1,
-        },
+const UserSchema = new mongoose.Schema(
+  {
+    username: {
+      type: String,
+      require: true,
+      minLength: 3,
+      max: 20,
+      unique: true,
     },
+    email: {
+      type: String,
+      required: [true, "Please enter an email"],
+      unique: true,
+      lowercase: true,
+      validate: [isEmail, "Please enter a valid email"],
+    },
+    password: {
+      type: String,
+      required: [true, "Please enter an password"],
+      minLength: [8, "Password shoul have more than 8 characters"],
+    },
+    profilePicture: {
+      type: String,
+      default: "",
+    },
+    coverPicture: {
+      type: String,
+      default: "",
+    },
+    followers: {
+      type: Array,
+      default: [],
+    },
+    followings: {
+      type: Array,
+      default: [],
+    },
+    isAdmin: {
+      type: Boolean,
+      default: false,
+    },
+    desc: {
+      type: String,
+      max: 50,
+    },
+    city: {
+      type: String,
+      max: 50,
+    },
+    from: {
+      type: String,
+      max: 50,
+    },
+    relationship: {
+      type: Number,
+      enum: [1, 2, 3],
+    },
+  },
+  { timestamps: true }
 );
 
-module.exports = mongoose.model("UserModel", User);
+module.exports = mongoose.model("User", UserSchema);
