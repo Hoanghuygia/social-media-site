b09665aaf22fccc5dd0747797bb5d22bd3775396
HTNT0117 <ngoctram.bie.u@gmail.com>
2024-06-08 23:58:43 +0700

    commit -m "Imeehe"
    erge branch 'master' into ngtram
    
    ^WDwdw[p\
    ii# Please enter a commit message to explain why this merge is necessary,

--- api/src/app/controllers/UserController.js
+++ api/src/app/controllers/UserController.js
@@ -1,7 +1,6 @@
 const User = require('../models/user');
 
 class UserController {
-
     getUser = async (req, res) => {
         try {
           const user = await User.findOne({ username: req.params.username });
@@ -21,6 +20,8 @@ class UserController {
           // Prevent updating username and email
           delete updateFields.username;
           delete updateFields.email;
+          delete updateFields.followers;
+          delete updateFields.followings;
           
           const updatedUser = await User.findOneAndUpdate(
             { username: req.params.username },
@@ -49,6 +50,90 @@ class UserController {
           res.status(500).json({ message: err.message });
         }
       };
+
+      // New functions
+
+      getFollowers = async (req, res) => {
+        try {
+          const user = await User.findOne({ username: req.params.username }).populate('followers.follower_id', 'username');
+          if (!user) {
+            return res.status(404).json({ message: 'User not found' });
+          }
+          res.status(200).json(user.followers);
+        } catch (err) {
+          res.status(500).json({ message: err.message });
+        }
+      };
+
+      getFollowings = async (req, res) => {
+        try {
+          const user = await User.findOne({ username: req.params.username }).populate('followings.following_id', 'username');
+          if (!user) {
+            return res.status(404).json({ message: 'User not found' });
+          }
+          res.status(200).json(user.followings);
+        } catch (err) {
+          res.status(500).json({ message: err.message });
+        }
+      };
+
+      addFollower = async (req, res) => {
+        try {
+          const user = await User.findOne({ username: req.params.username });
+          const follower = await User.findById(req.body.follower_id);
+          if (!user || !follower) {
+            return res.status(404).json({ message: 'User or follower not found' });
+          }
+          user.followers.push({ follower_id: req.body.follower_id });
+          await user.save();
+          res.status(200).json({ message: 'Follower added successfully' });
+        } catch (err) {
+          res.status(500).json({ message: err.message });
+        }
+      };
+
+      removeFollower = async (req, res) => {
+        try {
+          const user = await User.findOne({ username: req.params.username });
+          if (!user) {
+            return res.status(404).json({ message: 'User not found' });
+          }
+          user.followers = user.followers.filter(follower => follower.follower_id.toString() !== req.body.follower_id);
+          await user.save();
+          res.status(200).json({ message: 'Follower removed successfully' });
+        } catch (err) {
+          res.status(500).json({ message: err.message });
+        }
+      };
+
+      addFollowing = async (req, res) => {
+        try {
+          const user = await User.findOne({ username: req.params.username });
+          const following = await User.findById(req.body.following_id);
+          if (!user || !following) {
+            return res.status(404).json({ message: 'User or following not found' });
+          }
+          user.followings.push({ following_id: req.body.following_id });
+          await user.save();
+          res.status(200).json({ message: 'Following added successfully' });
+        } catch (err) {
+          res.status(500).json({ message: err.message });
+        }
+      };
+
+      removeFollowing = async (req, res) => {
+        try {
+          const user = await User.findOne({ username: req.params.username });
+          if (!user) {
+            return res.status(404).json({ message: 'User not found' });
+          }
+          user.followings = user.followings.filter(following => following.following_id.toString() !== req.body.following_id);
+          await user.save();
+          res.status(200).json({ message: 'Following removed successfully' });
+        } catch (err) {
+          res.status(500).json({ message: err.message });
+        }
+      };
 }
 
-module.exports = { getUserById };
+module.exports = new UserController();
--- api/src/routes/user.js
+++ api/src/routes/user.js
@@ -2,13 +2,11 @@ const express = require('express');
 const router = express.Router();
 const { getUserById } = require('../app/controllers/UserController'); 
 
-<<<<<<< HEAD
+
 const userController = require('../app/controllers/UserController');
 
 router.get('/:username', userController.getUser);
 router.put('/:username', userController.updateUser);
-=======
-router.get('/:id', getUserById);
->>>>>>> master
+router.get('/followers/:username', userController.getFollowers);
 
 module.exports = router;
