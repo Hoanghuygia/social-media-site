68173a461ca7fe9713475248d2aab89188d9654f
HTNT0117 <ngoctram.bie.u@gmail.com>
2024-06-16 13:42:04 +0700

    update profile picture and posts count

--- client/src/pages/profile/StaturBar.jsx
+++ client/src/pages/profile/StaturBar.jsx
@@ -1,13 +1,32 @@
-import { useState } from "react";
+import { useState, useEffect } from "react";
 import avatar from "./../../../public/img/avatar.png";
 import { PiVideoBold } from "react-icons/pi";
 import { FaRegImage } from "react-icons/fa";
-
 import StatusModal from "./StatusModal";
+
+const fetchUserData = async (username) => {
+  try {
+    const token = localStorage.token;
+    const response = await fetch(`https://sugar-cube.onrender.com/user/${username}`, {
+      headers: {
+        'Authorization': `Bearer ${token}` // Example of adding an authorization header
+      }
+    });
+    if (!response.ok) {
+      throw new Error("Network response was not ok");
+    }
+    const data = await response.json();
+    return data;
+  } catch (error) {
+    console.error("There was a problem with the fetch operation:", error);
+  }
+};
 
 export default function StatusBar() {
   const [isModalOpen, setIsModalOpen] = useState(false);
   const [statuses, setStatuses] = useState([]);
+  const [userData, setUserData] = useState({});
+  const user = localStorage.username;
 
   const handleOpenModal = () => {
     setIsModalOpen(true);
@@ -21,6 +40,15 @@ export default function StatusBar() {
     setStatuses([newStatus, ...statuses]);
   };
 
+  useEffect(() => {
+    const fetchUser = async () => {
+      const data = await fetchUserData(user);
+      console.log(data);
+      setUserData(data);
+    };
+    fetchUser();
+  }, [user]);
+
   return (
     <>
       <StatusModal
@@ -32,7 +60,7 @@ export default function StatusBar() {
         <div className="bg-[#F3E9FF] py-4 px-2 rounded-full w-full flex flex-row justify-center items-center gap-7">
           <div className="relative w-[50px] h-[50px] rounded-full overflow-hidden flex justify-center">
             <img
-              src={avatar}
+              src={userData.profilePicture || avatar}
               alt="avatar"
               className="w-full h-full object-cover"
             />
@@ -51,14 +79,12 @@ export default function StatusBar() {
           </div>
         </div>
 
-        
-
         <div className="mt-4 w-full max-w-lg">
           {statuses.map((status, index) => (
             <div key={index} className="bg-white p-4 mb-2 border border-gray-200 rounded-md shadow-sm">
               <p>{status.status}</p>
-              {status.image && <img src={URL.createObjectURL(status.image)} alt="status" className="mt-2" />}
-              {status.video && <video controls src={URL.createObjectURL(status.video)} className="mt-2" />}
+              {status.image && <img src={status.image} alt="status" className="mt-2" />}
+              {status.video && <video controls src={status.video} className="mt-2" />}
             </div>
           ))}
         </div>
